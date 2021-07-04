f0e9abc8e85535a904695a71e5a3fb3a94af07c6
Mohammad Ali Bazzazi <81759694+bazzazi@users.noreply.github.com>
2021-07-04 19:26:13 +0430

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+###############          ##########        #######   #######        #########      #######       #
+#              #        #          #             #         #       #         #           #
+#               #       #          #            #         #        #         #          #        #
+#              #        #          #           #         #         #         #         #         #
+###############         ############          #         #          ###########        #          #
+#              #        #          #         #         #           #         #       #           #
+#               #       #          #        #         #            #         #      #            #
+#              #        #          #       #         #             #         #     #             #
+###############         #          #      #######    #######       #         #    #######        #
+
+# Developer: Mohammad Ali Bazzazi (me)
+
+########################### START ###########################
+# Importing Libraries
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.lang.builder import Builder
+
+# Import image.kv file
+Builder.load_file("image.kv")
+
+# Create a class
+class MyLayout(Widget):
+    pass
+
+
+# Create main app class 
+class MyApp(App):
+    # Set background to White
+    Window.clearcolor = (1, 1, 1, 1)
+    def build(self):
+        return MyLayout()
+
+
+# Run App
+if __name__ == "__main__":
+    MyApp().run()   
+########################### END ###########################
